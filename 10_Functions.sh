0973c37ed3a1540fd6f061aea62b88188531e55e
vivekswaroopk <vivekswaroop1998@gmail.com>
2025-08-17 17:37:43 +0530

    commit

---
+++ 10_Functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ID= $(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "Error : $2 Failed"
+    else
+        echo "$2 Passed"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "Please run with root access"
+else
+    echo "You are root user"
+fi
+
+yum install mysql -y
+VALIDATE $? "Installing MySQL"
+
+yum install git -y
+VALIDATE $? "Installing GIT"
