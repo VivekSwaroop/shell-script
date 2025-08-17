5223584802d88e134c27fa67f15b09ebd406080e
vivekswaroopk <vivekswaroop1998@gmail.com>
2025-08-17 15:22:58 +0530

    commit

---
+++ 05_variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "pls enter ur username"
+
+read -s USERNAME
+
+echo "pls enter ur password"
+
+read -s PASSWORD
+
+echo "Entered username is ${USERNAME} and Password is ${PASSWORD}"
