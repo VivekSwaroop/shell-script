518cc8ce505ea2180644caab1a274940372acf93
vivekswaroopk <vivekswaroop1998@gmail.com>
2025-08-17 15:30:07 +0530

    commit

---
+++ 07_Arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Fruits=["Apple" "Banana" "Mango"]
+echo "First value : ${Fruits[0]}"
+echo "Second value : ${Fruits[1]}"
+echo "Third value : ${Fruits[2]}"
+echo "Total value : ${Fruits[@]}"
