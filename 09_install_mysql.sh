4255086bfc0975c2e04849ef59dec1434db5fa84
vivekswaroopk <vivekswaroop1998@gmail.com>
2025-08-17 17:25:19 +0530

    commit

--- 09_install_mysql.sh
+++ 09_install_mysql.sh
@@ -4,28 +4,28 @@ ID=$(id -u)
 
 if($ID -ne 0)
 then
-    echo ("Please run with root access")
+    echo "Please run with root access"
     exit 1
 else
-    echo ("You are root user")
+    echo "You are root user"
 fi
 
 yum install mysql -y
 
 if($? -ne 0)
 then 
-    echo ("ERROR Installing MySQL is failed")
+    echo "ERROR Installing MySQL is failed"
     exit 1
 else
-    echo ("MySQL installed")
+    echo "MySQL installed"
 fi
 
 yum install git -y
 
 if($? -ne 0)
 then 
-    echo ("ERROR Installing GIT is failed")
+    echo "ERROR Installing GIT is failed"
     exit 1
 else
-    echo ("GIT installed")
+    echo "GIT installed"
 fi
